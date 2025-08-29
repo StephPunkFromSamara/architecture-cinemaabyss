541bf0e8b6581b478eba898d35dda626d207d127
stepankirpichenkov <skirpichenkov@diasoft.ru>
2025-08-29 15:18:36 +0400

    Комит с заданиями 1,2 + правка под ответа events под автотесты

--- src/microservices/events/main.py
+++ src/microservices/events/main.py
@@ -59,3 +59,10 @@ def create_event(event_type: str, payload: dict):
 def health():
     return {"status": True}
 
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8082)
